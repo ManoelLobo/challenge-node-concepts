e1a91fd09aaf47eb6ae00a9969800693d88ab35d
Manoel <manoel.lobo@gmail.com>
2020-04-22 20:23:11 -0300

    feat: DELETE /repositories/:id

--- src/app.js
+++ src/app.js
@@ -50,7 +50,19 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Invalid Repository" });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(200);
 });
 
 app.post("/repositories/:id/like", (request, response) => {
